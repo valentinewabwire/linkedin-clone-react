755b3476a900f38d13cdc061352c3a32ee1cc019
valentinewabwire <valentine.w.maita@gmail.com>
2022-03-04 17:16:30 +0300

    header and home page add

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+
+function Home({ props }) {
+  return (
+    <div>
+      <Header />
+      Home
+    </div>
+  );
+}
+
+export default Home;
