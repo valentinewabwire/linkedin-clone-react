24ac3925a76146fd17d374f28e4c1c5c26ebcd37
valentinewabwire <valentine.w.maita@gmail.com>
2022-03-14 17:51:20 +0300

    header addition

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,33 @@ function Home({ props }) {
   return (
     <div>
       <Header />
-      Home
+      <Container>
+        <Section>
+          <h5>
+            <a>Hiring in a hurry?</a>
+          </h5>
+          <p>Find talented pros in record time and keep business running</p>
+        </Section>
+      </Container>
     </div>
   );
 }
-
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+const Section = styled.div`
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+`;
+const Content = styled.div`
+  max-width: 1126px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 export default Home;
